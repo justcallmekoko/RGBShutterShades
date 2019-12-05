d61238b3ae5eac4fc337dcc14abc9ee8b548e833
Just Call Me Koko <25190487+justcallmekoko@users.noreply.github.com>
2019-12-04 20:43:08 -0500

    Update RGBShutterShades.ino

--- RGBShutterShades.ino
+++ RGBShutterShades.ino
@@ -11,8 +11,6 @@ ButtonInput buttonObj;
 void setup() {
   Serial.begin(115200);
 
-  //webInterfaceObj.initWebInterface();
-
   buttonObj.initButtonInput();
   
   strip.begin();
@@ -30,83 +28,5 @@ void setup() {
 void loop()
 {
   buttonObj.checkPatternChange();
-  //webInterfaceObj.main();
-
-  //patternObj.initPattern(DASH);
-  //patternObj.initPattern(PULSE);
   patternObj.initPattern(patternObj.currentPattern);
-  //patternObj.Rainbow2();
-  //patternObj.initPattern(TWINKLE);
-  //patternObj.colorWipe(strip.Color(255, 0, 0), 50); // Red
-
-  //theaterChase(strip.Color(127, 127, 127), 50); // White
-  //theaterChase(strip.Color(127, 0, 0), 50); // Red
-  //theaterChase(strip.Color(0, 0, 127), 50); // Blue
-
-  //rainbow(20);
-  //theaterChaseRainbow(50);
-}
-
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-
-      delay(wait);
-
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-      }
-      strip.show();
-
-      delay(wait);
-
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
